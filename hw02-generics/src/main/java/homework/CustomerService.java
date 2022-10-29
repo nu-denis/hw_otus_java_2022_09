08c713832023a1d682501a2c8eb4289f1a51050c
Denis Nuriakhmetov <de.nuriakhmetov@hh.ru>
2022-10-29 23:34:41 +0300

    hw02-generics renaming

--- hw02-generics/src/main/java/homework/CustomerService.java
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -3,16 +3,15 @@ package homework;
 
 import java.util.AbstractMap;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Map;
 
 public class CustomerService {
-    private final ArrayList<AbstractMap.SimpleEntry<Customer, String>> customers;
-    private Map.Entry<Customer, String> smallerCustomer;
+    private final ArrayList<AbstractMap.SimpleEntry<Customer, String>> customersData;
+    private Map.Entry<Customer, String> smallestCustomerData;
 
     public CustomerService() {
-        this.customers = new ArrayList<>();
-        this.smallerCustomer = null;
+        this.customersData = new ArrayList<>();
+        this.smallestCustomerData = null;
     }
 
     //todo: 3. надо реализовать методы этого класса
@@ -20,25 +19,26 @@ public class CustomerService {
 
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return new AbstractMap.SimpleEntry<>(new Customer(smallerCustomer.getKey()), smallerCustomer.getValue());
+        return new AbstractMap.SimpleEntry<>(
+            new Customer(smallestCustomerData.getKey()), smallestCustomerData.getValue()
+        );
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        customers.sort(Comparator.comparing(o -> o.getKey().getScores()));
         int customerScore = Math.toIntExact(customer.getScores());
-        for (Map.Entry<Customer, String> el : customers) {
-            if (customerScore < el.getKey().getScores()) {
-                return new AbstractMap.SimpleEntry<>(new Customer(el.getKey()), el.getValue());
+        for (Map.Entry<Customer, String> customerData : customersData) {
+            if (customerScore < customerData.getKey().getScores()) {
+                return new AbstractMap.SimpleEntry<>(new Customer(customerData.getKey()), customerData.getValue());
             }
         }
         return null;
     }
 
     public void add(Customer customer, String data) {
-        AbstractMap.SimpleEntry<Customer, String> element = new AbstractMap.SimpleEntry<>(customer, data);
-        customers.add(element);
-        if (smallerCustomer == null || smallerCustomer.getKey().getScores() > customer.getScores()) {
-            smallerCustomer = element;
+        AbstractMap.SimpleEntry<Customer, String> newCustomerData = new AbstractMap.SimpleEntry<>(customer, data);
+        customersData.add(newCustomerData);
+        if (smallestCustomerData == null || smallestCustomerData.getKey().getScores() > customer.getScores()) {
+            smallestCustomerData = newCustomerData;
         }
     }
 }
