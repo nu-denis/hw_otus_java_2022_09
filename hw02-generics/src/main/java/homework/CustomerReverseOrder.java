8f691fea2e5bc696d1cf7924394fa00d2513bcd6
Denis Nuriakhmetov <de.nuriakhmetov@hh.ru>
2022-10-30 23:26:37 +0300

    hw02-generics replace public -> private

--- hw02-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw02-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -4,7 +4,7 @@ package homework;
 import java.util.LinkedList;
 
 public class CustomerReverseOrder {
-    public final LinkedList<Customer> customers = new LinkedList<>();
+    private final LinkedList<Customer> customers = new LinkedList<>();
 
     //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
